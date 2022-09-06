eaf1ec072180fc01160f02dd4b159eb481c5c1f9
6hhhi <liuyuxiaokk@gmail.com>
2022-09-06 22:07:10 +0800

    初始可运行

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 import logging
 import os
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -17,24 +16,23 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 url = r'https://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp'
-#driver_path = ChromeDriverManager().install()
+#driver_path = ChromeDriverManager().install()  #暂时不生效作保留
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')  #窗口显示
 service = Service((ChromeDriverManager().install()))
-
 
 def run(username: str, password: str):
     driver = webdriver.Chrome(service=service, options=chrome_options)
     #driver = webdriver.Chrome()
     driver.get(url)
-    time.sleep(2)
+    time.sleep(5)
     username_element = driver.find_element(By.ID, 'username')
     username_element.send_keys(username)
     password_element = driver.find_element(By.ID, 'password')
     password_element.send_keys(password)
     driver.find_element(By.NAME, 'submit').click()
-    time.sleep(2)
-    driver.switch_to.window(driver.window_handles[-1])  # ? 窗口切换
+    time.sleep(5)
+    #driver.switch_to.window(driver.window_handles[-1])  # ? 窗口切换
     '''
     try:
         driver.find_element(By.PARTIAL_LINK_TEXT, '我知道了').click()
@@ -45,19 +43,21 @@ def run(username: str, password: str):
     js = 'go_sub();document.querySelector("label.weui-cell.weui-cell_active.weui-check__label").click();save()'
     driver.execute_script(js)
     '''
-    driver.find_element(By.XPATH, '//*[@id="rbxx_div"]/div[27]').click()# 点击提交填报信息
-    time.sleep(5)
-    #driver.find_element(By.XPATH, '//*[@id="qrxx_div"]/div[2]/div[26]/label/div[1]/i').click() #点击已核实以上数据
-    driver.find_element(By.XPATH, '//*[@id="qrxx_div"]/div[2]/div[26]').click()  # 点击已核实以上数据
-    #driver.find_element(By.XPATH, '//*[@id="save_div"]').click() # 点击确认提交
+    driver.find_element(By.XPATH, '//*[@id="rbxx_div"]/div[17]').click()  #同下
+    #driver.find_element(By.XPATH, '//*[@id="rbxx_div"]/div[27]').click()# 点击提交填报信息
+    time.sleep(2)
+    driver.find_element(By.XPATH, '//*[@id="qrxx_div"]/div[2]/div[11]').click()  #点击已核实以上数据
+    #driver.find_element(By.XPATH, '//*[@id="qrxx_div"]/div[2]/div[26]').click()  # 点击已核实以上数据
     driver.find_element(By.XPATH, '//*[@id="save_div"]').click()  # 点击确认提交
-    time.sleep(5)
+    #driver.find_element(By.XPATH, '//*[@id="save_div"]').click()  # 点击确认提交
+
+    time.sleep(2)
     driver.close()
     logger.info(f'{username} 已完成填报')
 
 
 def yqtb(students: list):
-#def yqtb():
+    print(students)
     logger.info('开始执行填报...')
     for username, password in students:
         run(username, password)
@@ -66,6 +66,5 @@ def yqtb(students: list):
 
 if __name__ == '__main__':
     students = json.loads(config)
-#logger.info(f'加载的用户列表: {[username for username, _ in students]}')
+    #logger.info(f'加载的用户列表: {[username for username, _ in students]}')
     yqtb(students)
-
